6b229d134008038e4b4117c5108e670ed85c3fca
Alejandra Rodriguez <alerodriguezabella@gmail.com>
2022-09-15 10:14:29 +0200

    home page done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import "../App.css";
 import itinerariesPic from '../assets/itineraries.png'
 import plan from '../assets/plan.png'
+import travelTips from '../assets/travelTips.jpg'
 import ComplexButton from "../components/ComplexButton";
 import {Link} from 'react-router-dom'
 
@@ -8,23 +9,31 @@ function Home() {
   return (
     <div style={{flex:1}}>
 
-      <div style={{height: '50%'}}>
+      <div style={{height: '34%'}}>
         <Link to='/itineraries'>
           <ComplexButton 
-            title='See all itineraries'
+            title='Explore itineraries'
             imageUrl={itinerariesPic}
           />
         </Link>
       </div>
 
-      <div style={{height: '50%'}}>
+      <div style={{height: '33%'}}>
       <Link to='/new-itinerary'>
         <ComplexButton 
-          title='Create a new itinerary'
+          title='Create an itinerary'
           imageUrl={plan}
         />
       </Link>
+      </div>
 
+      <div style={{height: '33%'}}>
+      <Link to='/traveltips'>
+        <ComplexButton 
+          title='Explore travel tips'
+          imageUrl={travelTips}
+        />
+      </Link>
       </div>
 
     </div>
